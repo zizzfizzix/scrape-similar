ab4a1524dcf66daa3c520dc6cf2aab431eb7aab5
Kuba Serafinowski <kuba.serafinowski@gmail.com>
2025-04-21 19:21:16 +0100

    feat: add custom logo files
    Signed-off-by: Kuba Serafinowski <kuba.serafinowski@gmail.com>

--- src/manifest.ts
+++ src/manifest.ts
@@ -11,13 +11,13 @@ export default defineManifest({
   manifest_version: 3,
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu6ONFPt3ct0twLsACoeS7JjXkpEznkKkuh9uTApNa8EToDneOS0JPcD4cD3KEaO+SlsLPjU0JVkqX4/XBM6meSw4UIRydJCEf4UbCD+PR61FyuPq7Gp67Kd9jF0Oet7a/4nE7cNrBhz98CwifvI4MlIQWWK5jwwpAhVf0rQDjpG38H8+t2blGTlH8aP4+S/74qLN2fLILP7rIiovq4uvskBjJ4RwM3d6azdp0eCqrBDF6xAfTqp/8k+ZZ6wzGMsmKQI+yAdRhpsa0gfcFNnnu+ATErKHCL0seQB4kE4pHmA4q0ii70z6nOYO/NlT2wEmOvtfouuhvBGOGbChv9sHgQIDAQAB',
   icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
-    48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
+    16: 'img/logo-light-16.png',
+    32: 'img/logo-light-32.png',
+    48: 'img/logo-light-48.png',
+    128: 'img/logo-light-128.png',
   },
   action: {
-    default_icon: 'img/logo-48.png',
+    default_icon: 'img/logo-light-48.png',
     default_title: 'Open Modern Scraper',
   },
   background: {
