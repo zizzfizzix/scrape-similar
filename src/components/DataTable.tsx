f75438221c62a8b9433fb3903976db755a8a9610
Kuba Serafinowski <kuba.serafinowski@gmail.com>
2025-05-26 18:36:10 +0100

    feat: justify-center the pagination controls
    Signed-off-by: Kuba Serafinowski <kuba.serafinowski@gmail.com>

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -209,7 +209,7 @@ const DataTable: React.FC<DataTableProps> = ({
         </Table>
       </div>
       {/* Pagination Controls */}
-      <div className="flex items-center justify-end gap-2 mt-4">
+      <div className="flex items-center justify-center gap-2 mt-4">
         <Button
           variant="outline"
           size="sm"
