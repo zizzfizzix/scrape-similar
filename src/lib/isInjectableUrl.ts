3d4a400ec6d2e6cd2fd28fe2036a1a9a3ccacb2b
Kuba Serafinowski <kuba.serafinowski@gmail.com>
2025-07-06 19:37:40 -0400

    fix: mark old webstore urls as unsupported
    Signed-off-by: Kuba Serafinowski <kuba.serafinowski@gmail.com>

--- src/lib/isInjectableUrl.ts
+++ src/lib/isInjectableUrl.ts
@@ -1,4 +1,8 @@
 export const isInjectableUrl = (url?: string): boolean => {
   if (!url) return false
-  return !url.startsWith('chrome://') && !url.startsWith('https://chromewebstore.google.com/')
+  return (
+    !url.startsWith('chrome://') &&
+    !url.startsWith('https://chromewebstore.google.com/') &&
+    !url.match('^https://chrome.google.com/(.+/)?webstore/')
+  )
 }
