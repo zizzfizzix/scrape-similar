88e669b6ef48a715c1c2b5c701982c4c60cd55a0
Kuba Serafinowski <kuba.serafinowski@gmail.com>
2025-07-10 07:48:34 -0400

    ci: add github workflows
    Signed-off-by: Kuba Serafinowski <kuba.serafinowski@gmail.com>

--- src/zip.js
+++ src/zip.js
@@ -1,10 +1,22 @@
+import { execSync } from 'child_process'
 import gulp from 'gulp'
 import zip from 'gulp-zip'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
-const manifest = require('../build/manifest.json')
+const packageData = require('../package.json')
+
+// Check if current commit is at a tag
+let versionSuffix = ''
+try {
+  execSync('git describe --tags --exact-match', { stdio: 'pipe' }).toString().trim()
+  // If this succeeds, we're at a tag, no suffix needed
+} catch (e) {
+  // Not at a tag, append short commit hash
+  const hash = execSync('git rev-parse --short HEAD', { stdio: 'pipe' }).toString().trim()
+  versionSuffix = `+g${hash}`
+}
 
 gulp
   .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
+  .pipe(zip(`${packageData.name}-v${packageData.version}${versionSuffix}.zip`))
   .pipe(gulp.dest('package'))
